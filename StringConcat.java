1c630b9619c6af5957523ce82e024f03e7442859
Tony Syv <11375364+TonySyv@users.noreply.github.com>
2025-08-31 18:50:51 +0800

    StringConcat.java

---
+++ StringConcat.java
@@ -0,0 +1,11 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = " World";
+
+        // Concatenation of s1 and s2
+        s1 = s1 + s2;
+
+        System.out.println(s1);
+    }
+}
